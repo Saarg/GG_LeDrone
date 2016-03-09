c9cc96d8e50ff0371145416902052af017f33243
BuildTools <milsonneau.jean@free.fr>
2016-03-09 19:12:22 +0100

    ajout d'options arch et build au script build_SDK

--- build_SDK.sh
+++ build_SDK.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-sudo apt-get install git wget automake autoconf libtool yasm nasm phablet-tools
+if [[ $1 == "help" ]]; then
+    echo "Pour télécharger et compiler le SDK parrot"
+    echo "Sous debian(apt-get):"
+    echo "    bash build_SDK.sh"
+    echo "Sous Arch Linux(pacman & yaourt):"
+    echo "    bash build_SDK.sh arch"
+    echo "Pour juste télécharger les sources et les compiler:"
+    echo "    bash build_SDK.sh build"
+    exit
+fi
+
+if [[ $1 == "arch" ]]; then
+    sudo pacman -S git wget automake autoconf libtool yasm nasm
+    sudo yaourt -S phablet-tools
+elif [[ $1 != "build" ]]; then
+    sudo apt-get install git wget automake autoconf libtool yasm nasm phablet-tools
+fi
 
 repo init -u https://github.com/Parrot-Developers/arsdk_manifests.git
 repo sync
