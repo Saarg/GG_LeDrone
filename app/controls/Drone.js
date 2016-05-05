5f46d7077f3897b5df91e24b3d34b555bdb110ef
y0l0h4nn <yo.creach@gmail.com>
2016-05-05 13:05:45 +0200

    runPath, pas sûr de la sortie

--- app/controls/Drone.js
+++ app/controls/Drone.js
@@ -1,14 +1,14 @@
 "use strict"
 
 /*
-*
-* singleton de la classe drone.
-*
-*/
+ *
+ * singleton de la classe drone.
+ *
+ */
 var nodeSumo = require('node-sumo');
-var fs       = require('fs');
+var fs = require('fs');
 
-var Image    = require('./Image.js');
+var Image = require('./Image.js');
 
 var Drone = function(pos) {
     console.log("Création de l'instance");
@@ -27,12 +27,26 @@ var Drone = function(pos) {
     this.img = new Image();
 
     // ENUMS
-    this.jumps = {hight: 0, long: 1};
-    this.directions = {forward: 0, backward: 1, left: 2, right: 3};
-    this.postures = {postureUnknown: -1, postureStanding: 0, postureJumper: 1, postureKicker: 2, postureStuck: 3};
+    this.jumps = {
+        hight: 0,
+        long: 1
+    };
+    this.directions = {
+        forward: 0,
+        backward: 1,
+        left: 2,
+        right: 3
+    };
+    this.postures = {
+        postureUnknown: -1,
+        postureStanding: 0,
+        postureJumper: 1,
+        postureKicker: 2,
+        postureStuck: 3
+    };
 };
 
-Drone.prototype.connect = function (callback) {
+Drone.prototype.connect = function(callback) {
     // Connect
     this.drone.connect(function() {
         var d = Drone.getInstance();
@@ -79,42 +93,42 @@ Drone.prototype.connect = function (callback) {
 
 };
 
-Drone.prototype.move = function (dir, speed) {
+Drone.prototype.move = function(dir, speed) {
     this.stop();
-    if(dir == this.directions.forward)
+    if (dir == this.directions.forward)
         this.drone.forward(speed);
-    else if(dir == this.directions.backward)
+    else if (dir == this.directions.backward)
         this.drone.backward(speed);
-    else if(dir == this.directions.left)
+    else if (dir == this.directions.left)
         this.drone.left(speed);
-    else if(dir == this.directions.right)
+    else if (dir == this.directions.right)
         this.drone.right(speed);
 };
 
-Drone.prototype.stop = function () {
+Drone.prototype.stop = function() {
     this.drone.stop();
 };
 
-Drone.prototype.jump = function (type) {
+Drone.prototype.jump = function(type) {
     console.error("les sauts ne sont pas disponible GG est en panne");
     //this.drone.animationsLongJump();
 };
 
-Drone.prototype.tap = function () {
+Drone.prototype.tap = function() {
     this.drone.animationsTap();
 };
 
-Drone.prototype.getPicture = function () {
+Drone.prototype.getPicture = function() {
     console.log(this.buf);
-    fs.writeFile('img', this.img.getData(),function(err){
-        if(err) throw err;
+    fs.writeFile('img', this.img.getData(), function(err) {
+        if (err) throw err;
     });
 };
 
 Drone.instance = null;
 
-Drone.getInstance = function(){
-    if(this.instance === null){
+Drone.getInstance = function() {
+    if (this.instance === null) {
         this.instance = new Drone();
     }
     return this.instance;
--- app/controls/DroneHandler.js
+++ app/controls/DroneHandler.js
@@ -1,10 +1,10 @@
 'use strict'
-var Drone   = require("./Drone.js");
-var Office  = require("./Office.js");
-var Ark     = require("./Ark.js");
+var Drone = require("./Drone.js");
+var Office = require("./Office.js");
+var Ark = require("./Ark.js");
 
 var DroneHandler = function(offices) {
-    this.offices=offices;
+    this.offices = offices;
     //this.offices.prototype=new Office() //c'est fait en mode crassou : lors de l'instanciation d'un office, il s'ajoute au handler A CHANGER
     //TODO chargement de la liste des bureaux
     //this.base = this.offices[0];
@@ -18,73 +18,75 @@ DroneHandler.prototype.droneConnect = function() {
     drone.connect();
 };
 
-DroneHandler.prototype.dijkstra	= function(){ //boucle infinie
-    var sumMarks=0;
+DroneHandler.prototype.dijkstra = function() { //boucle infinie
+    var sumMarks = 0;
     var len = this.offices.length;
-    var marks = [];        //vecteur de marquage
-    var distance = [];     //vecteur distances
-    var incArks = [];      //vecteur arcs entrants
+    var marks = []; //vecteur de marquage
+    var distance = []; //vecteur distances
+    var incArks = []; //vecteur arcs entrants
 
-    for (var i = 0; i < len; i++) {			/*-------------------------------------------------*/
-		marks[i] = 0;
-		distance[i] = 99999999;				/*Initialisation des sommets*/
+    for (var i = 0; i < len; i++) { /*-------------------------------------------------*/
+        marks[i] = 0;
+        distance[i] = 99999999; /*Initialisation des sommets*/
     }
 
-	var pos = this.offices.indexOf(this.drone.position);		/*Sommet de départ*/
-  distance[pos] = 0;
-									/*-------------------------------------------------*/
+    var pos = this.offices.indexOf(this.drone.position); /*Sommet de départ*/
+    //  var pos = 0; //à remplacer par la ligne, test drone batterie à plat
 
-  while (sumMarks < len) {  		/*tant que tous les sommets ne sont pas traités*/
-		sumMarks = 0;
-		var officeIndex;
-		var min = 9999999999;
-									/*-------------------------------------------------*/
-    for (var i in this.offices){
-			if ((marks[i]==0) && (distance[i] < min)) {
-				min = distance[i];
-				officeIndex = i;		/*Recherche du sommet non marqué le plus proche*/
-			}
-		}
+    distance[pos] = 0;
+    /*-------------------------------------------------*/
 
-		var currOffice = this.offices[officeIndex]; //sommet a traite
-									/*-------------------------------------------------*/
+    while (sumMarks < len) { /*tant que tous les sommets ne sont pas traités*/
+        sumMarks = 0;
+        var officeIndex;
+        var min = 9999999999;
+        /*-------------------------------------------------*/
+        for (var i in this.offices) {
+            if ((marks[i] == 0) && (distance[i] < min)) {
+                min = distance[i];
+                officeIndex = i; /*Recherche du sommet non marqué le plus proche*/
+            }
+        }
+        var currOffice = this.offices[officeIndex]; //sommet a traite
 
-		for(var j in currOffice.arks){  /*Parcours des arcs de ce sommet*/
-			var nextOffice = currOffice.arks[j].getExtremity(currOffice);
-			var nextPos = this.offices.indexOf(nextOffice); 		/*On prends l'extremite de l'arc*/
-      if (distance[nextPos]> currOffice.arks[j].length + distance[officeIndex]) {
-				distance[nextPos] = distance[officeIndex] + currOffice.arks[j].length;
-				incArks[nextPos] = currOffice.arks[j]; //on stocke l'arc entrant dans un vecteur
+        /*-------------------------------------------------*/
+
+        for (var j in currOffice.arks) { /*Parcours des arcs de ce sommet*/
+            var nextOffice = currOffice.arks[j].getExtremity(currOffice);
+            var nextPos = this.offices.indexOf(nextOffice); /*On prends l'extremite de l'arc*/
+            if (distance[nextPos] > currOffice.arks[j].length + distance[officeIndex]) {
+                distance[nextPos] = distance[officeIndex] + currOffice.arks[j].length;
+                incArks[nextPos] = currOffice.arks[j]; //on stocke l'arc entrant dans un vecteur
             }
         }
 
-        marks[officeIndex] = 1; 	/*on marque le sommet quand on a fini*/
-		for(var l in marks) sumMarks += marks[l]; //on compte le nb de sommets marques
+        marks[officeIndex] = 1; /*on marque le sommet quand on a fini*/
+        for (var l in marks) sumMarks += marks[l]; //on compte le nb de sommets marques
     }
 
     for (var j in this.offices) {
-		this.offices[j].distance = distance[j];
-  }	//recopie de la distance dans chaque sommet (utile ?)
-
-	return incArks; //CECI N'EST PAS LE PATH
+        this.offices[j].distance = distance[j];
+    } //recopie de la distance dans chaque sommet (utile ?)
+    return incArks; //CECI N'EST PAS LE PATH
 }
 
 
-DroneHandler.prototype.convertPath = function(arksVec, destination){	/*convertit le vecteur d'arc en sortie de dijkstra*/
-	var pos = destination;
-	var start = this.drone.position;	/*renvoie le chemin à parcourir*/
-	this.path = [];
-	this.path.push(destination); 		//path contient l'arrivee
-	var posNbr = this.offices.indexOf(destination);
+DroneHandler.prototype.convertPath = function(arksVec, destination) { /*convertit le vecteur d'arc en sortie de dijkstra*/
+    var pos = destination;
+    var start = this.drone.position; /*renvoie le chemin à parcourir*/
+    this.path = [];
+    this.path.push(destination); //path contient l'arrivee
+    var posNbr = this.offices.indexOf(destination);
 
-	while (pos != start && arksVec[posNbr]) {
-		pos = arksVec[posNbr].getExtremity(pos);
-    if(pos==start) break;
-		posNbr = this.offices.indexOf(pos);
-		this.path.push(this.offices[posNbr]);		//apres cette boucle, path contient tous les sommets
-	}
+    while (pos != start && arksVec[posNbr]) {
+        pos = arksVec[posNbr].getExtremity(pos);
+        if (pos == start) break;
+        posNbr = this.offices.indexOf(pos);
+        this.path.push(this.offices[posNbr]); //apres cette boucle, path contient tous les sommets
+    }
+    this.path.push(this.drone.position);
+    this.path.reverse(); //CECI EST LE PATH
 
-	this.path.reverse(); //CECI EST LE PATH
 }
 
 
@@ -93,33 +95,61 @@ DroneHandler.prototype.convertPath = function(arksVec, destination){	/*convertit
 
 
 DroneHandler.prototype.findPath = function(req, res) {
-  arks=this.dijkstra();
-  this.path=this.convertPath(arks,req);
-  res.json({ success: true, message: "patate"})
+    arks = this.dijkstra();
+    this.path = this.convertPath(arks, req);
+    res.json({
+        success: true,
+        message: "patate"
+    })
 
 };
 
 
 
-DroneHandler.prototype.runPath = function(officeIndex,moveIndex) {
-  if(this.drone.connected && this.drone.ready && this.drone.posture==1){
-      var moves = this.path[officeIndex].findArk(officeIndex+1).moves;
+DroneHandler.prototype.runPath = function(req, res, officeIndex, moveIndex) {
+    if (this.drone.connected && this.drone.posture == 1) {
 
-      this.drone.move(moves[moveIndex],50);
+        console.log("\nentering runPath");
+        console.log("position : " + this.drone.position.researcher);
+        this.drone.stop();
 
-      setTimeout(function () {
-          if (moveIndex == moves.length) {
-              this.drone.position=this.path[officeIndex+1];
-              if(this.drone.position==this.destination){
-                return 0;
-              }
-              this.runPath(officeIndex+1,0);
-            }
-            else this.runPath(officeIndex,moveIndex+1);
-      }, 100);
-  }
+        var thisHandler = this; //this pas accessible dans le setTimeout;
+
+        if (this.drone.position != this.destination) {
+            var moves = this.path[officeIndex].findArk(this.path[officeIndex + 1]).moves;
+            this.drone.move(moves[moveIndex], 50);
+            setTimeout(function() {
+                thisHandler.pathRecursion(officeIndex, moveIndex, moves);
+            }, 500);
+        }
+        if (this.drone.position == this.destination) {
+            setTimeout(function() {
+                console.log("end")
+                thisHandler.endPath(req, res);
+            }, 500);
+        }
+    }
+}
+
+
+
+
+DroneHandler.prototype.endPath = function(req, res) {
+    console.log("entering endPath");
+    //json.res();
+    process.exit(); ////////////////////////////////////on peut faire comme ça ?
 };
 
+DroneHandler.prototype.pathRecursion = function(officeIndex, moveIndex, moves) {
+    console.log("move :" + moves[moveIndex])
+    if (moveIndex == moves.length - 1) {
+        this.drone.position = this.path[officeIndex + 1];
+        this.runPath(0, 0, officeIndex + 1, 0);
+    } else {
+        this.runPath(0, 0, officeIndex, moveIndex + 1);
+    }
+    console.log("end recursion");
+};
 
 DroneHandler.prototype.goHome = function(req, res) {
 
