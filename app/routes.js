de9a86034a8fd5aee765d116513167ac6a7bc93b
BuildTools <milsonneau.jean@free.fr>
2016-05-20 16:43:46 +0200

    route.js plus lisible

--- app/routes.js
+++ app/routes.js
@@ -8,24 +8,36 @@ var Drone = require ('./controls/Drone.js');
 var CERV = require ('./../config/CERV.js')
 
 module.exports = function(app) {
+    // Initialisation du DroneHandler ==========================================
     var DH = new DroneHandler();
 
+    // Gestion de la batterie ==================================================
+    Drone.low = function() {
+        console.log("niveau de batterie faible!");
+    };
+
+    Drone.critical = function() {
+        console.log("niveau de batterie critique!!!");
+    };
+
+    // Connect =================================================================
     /*Drone.connect(function(err, data) {
         console.log("GG est on");
     });*/
 
+    // Infos CERV ==============================================================
     app.get('/api/chercheurs', function(req, res) {
         // chercheurs
         res.json({success: true, message: "liste des chercheurs du CERV", chercheurs: CERV.chercheurs});
     });
 
-    // DroneHandler
+    // DroneHandler ============================================================
     app.post('/api/findPath', function(req, res){ DH.findPath(req, res); });
     app.post('/api/runPath', function(req, res){ DH.runPath(req, res) });
     app.post('/api/goHome', function(req, res){ DH.goHome(req, res) });
     app.post('/api/getResearshers', function(req, res){ DH.getResearshers(req, res) });
 
-    // Drone movement
+    // Drone movement ==========================================================
     app.get('/api/stop', function(req, res) {
         // none
         Drone.stop();
@@ -47,7 +59,7 @@ module.exports = function(app) {
         res.json({succes: true, message: "tap tap!"});
     });
 
-    // Animation
+    // Animations ==============================================================
     app.get('/api/animations', function(req, res) {
         // animations
         res.json({succes: true, message: "liste des animations dispo", animations: Drone.animations});
@@ -58,7 +70,7 @@ module.exports = function(app) {
         res.json({succes: true, message: "je fais l'animation"});
     });
 
-    // Image
+    // Image ===================================================================
     app.get('/api/picture', function(req, res) {
         var img = Drone.getPicture();
         res.json({succes: true, img: img});
