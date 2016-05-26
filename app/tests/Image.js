17284da1cd882917ca51c74b554be71a6debd872
y0h4nn <yo.creach@gmail.com>
2016-05-26 14:19:09 +0200

    Tests sur l'orientation en fonction de l'image ne fonctionne pas avec la nouvelle syntaxe et l'analyse direct du buffer ne fonctionne, j'ai du passer par l'écriture d'un fichier

--- app/tests/Image.js
+++ app/tests/Image.js
@@ -1,27 +1,103 @@
-var fs          = require('fs');
+var fs = require('fs');
 
-var Image       = require("./../controls/Image.js");
-var Color       = require("./../controls/Color.js");
+var Image = require("./../controls/Image.js");
+var Color = require("./../controls/Color.js");
+
+var Drone = require("./../controls/Drone.js");
+var DroneHandler = require("./../controls/DroneHandler.js");
+
+Drone.connect(function(err, data) {
+    //callback quand le drone se connecte
+    Drone.move(Drone.directions.left);
+    setTimeout(function() {
+        Drone.move(Drone.directions.right);
+        setTimeout(function() {
+            Drone.stop();
+            test();
+        }, 500);
+    }, 500);
+
+});
+var test = function() {
+    //var img = new Image();
+    //img.setData(Drone.getPicture())
+    var pic = Drone.getPicture();
+    var snap = new Image(640, 480);
+
+    snap.setData(pic);
+
+    // img.setData(fs.readFileSync(__dirname+"/test_photo.jpeg"), "image/jpeg");
+    console.log("\n\nRaw data: ");
+    console.log(snap.raw);
+    console.log("\n\ndata: ");
+    console.log(snap.getData());
+
+        var colorGrid = [{
+            r: 154,
+            g: 32,
+            b: 56,
+            x: 0,
+            y: 460,
+            w: 10,
+            h: 10
+        }, {
+            r: 154,
+            g: 32,
+            b: 56,
+            x: 360,
+            y: 460,
+            w: 10,
+            h: 10
+        }, {
+            r: 154,
+            g: 32,
+            b: 56,
+            x: 460,
+            y: 460,
+            w: 10,
+            h: 10
+        }, ]; // R G B X Y W H
+
+    console.log("\n\nColorGrid: ");
+    console.log(colorGrid);
+    /// l'imageAnalysis ne marche pas avec le buffer ...
+    // var retour = snap.imageAnalysis(colorGrid, 50);
+    //
+    // console.log("\n\nRetour: ");
+    // console.log(retour);
+    //
+    // var dir = snap.interprete(retour);
+    // console.log("direction a prendre :");
+    // console.log(dir);
+    fs.writeFile(__dirname + "/saved.jpeg", snap.getData(), function(err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+        ////////////////////
+        //fonctionne comme ça mais pas toujours et ça me semble pas normal de passer par là ...
+        ///////////////////
+
+        var img = new Image(640, 480);
+        img.setData(fs.readFileSync(__dirname + "/saved.jpeg"), "image/jpeg");
 
 
-var img = new Image();
 
-img.setData(fs.readFileSync(__dirname+"/img.jpeg"), "image/jpeg");
-console.log("\n\nRaw data: ");
-console.log(img.raw);
-console.log("\n\ndata: ");
-console.log(img.getData());
+        var retour = img.imageAnalysis(colorGrid, 50);
 
-var colorGrid = [
-    {r: 255, g: 0, b: 0, x: 0, y: 0, w: 10, h: 10},
-    {r: 255, g: 200, b: 0, x: 0, y: 0, w: 10, h: 10},
-    {r: 255, g: 200, b: 0, x: 0, y: 0, w: 10, h: 10},
-];// R G B X Y W H
+        console.log("\n\nRetour: ");
+        console.log(retour);
 
-console.log("\n\nColorGrid: ");
-console.log(colorGrid);
+        var dir = img.interprete(retour);
+        console.log("direction a prendre :");
+        console.log(dir);
+        ///////////////////////////
+    });
 
-var retour = img.imageAnalysis(colorGrid, 20);
 
-console.log("\n\nRetour: ");
-console.log(retour);
+
+
+    //
+
+}
